1d49de9f6ad20b8d938a6876ddbb265c7e0c7961
Carlos Antonio <you@example.com>
2017-03-06 20:08:19 -0300

    Criando Principal de Cadastros

---
+++ app/src/main/java/br/edu/iff/pooa20162/teste_lg/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package br.edu.iff.pooa20162.teste_lg.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+import br.edu.iff.pooa20162.teste_lg.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void chamaTelaCadastro(View v){
+        Toast.makeText(this,"",Toast.LENGTH_SHORT);
+    }
+}
