d4a23cb390dab0781675df86cdf21dd9476c4c38
Carlos Antonio <you@example.com>
2017-03-06 21:12:51 -0300

    Modificando a chamanda do Botao na tela principal

---
+++ app/src/main/java/br/edu/iff/pooa20162/teste_lg/activity/ListVActivity.java
@@ -0,0 +1,31 @@
+package br.edu.iff.pooa20162.teste_lg.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import br.edu.iff.pooa20162.teste_lg.R;
+
+public class ListVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_v);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/br/edu/iff/pooa20162/teste_lg/activity/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/teste_lg/activity/MainActivity.java
@@ -3,7 +3,9 @@ package br.edu.iff.pooa20162.teste_lg.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.content.Context;
+import android.content.Intent;
+
 
 
 import br.edu.iff.pooa20162.teste_lg.R;
@@ -16,7 +18,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void chamaTelaCadastro(View v){
-        Toast.makeText(this,"",Toast.LENGTH_SHORT);
+    public void chamaTelaCadastrop(View v){
+        Intent intentp = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intentp);
+
+    }
+
+    public void chamaTelaCadastrov(View v){
+
+        Intent intentv = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intentv);
+    }
+
+    private Context getContext(){
+        return this;
     }
 }
