6e6699f21a48d6bccfe7c2333a7f3efe0182c173
carlosiff <ocarlosnet@hotmail.com>
2017-03-11 13:48:15 -0300

    Modificando o Lista de proprietários

--- app/src/main/java/br/edu/iff/pooa20162/teste_lg/activity/ListActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/teste_lg/activity/ListActivity.java
@@ -1,8 +1,9 @@
 package br.edu.iff.pooa20162.teste_lg.activity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -24,8 +25,14 @@ public class ListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(ListActivity.this,CadastroActivity.class);
+                intent.putExtra("id",0);
+                intent.putExtra("nome","");
+                intent.putExtra("endereco","");
+                intent.putExtra("telefone","");
+                intent.putExtra("dataNasc","");
+
+                startActivity(intent);
             }
         });
     }
